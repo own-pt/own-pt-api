4b6a5331911239606b0c55938427bdea477f6c98
Fabricio Rosario <fchalub@br.ibm.com>
2015-10-23 14:26:48 -0200

    Added new "pointers" core.

--- solr/create.sh
+++ solr/create.sh
@@ -3,8 +3,12 @@
 #bin/solr delete -c suggestions
 #bin/solr delete -c votes
 #bin/solr delete -c audit
-bin/solr create -c wn -d ~/hack/wordnet/ownpt2/solr/wn-conf
-bin/solr create -c st -d ~/hack/wordnet/ownpt2/solr/st-conf
-bin/solr create -c suggestions -d ~/hack/wordnet/ownpt2/solr/suggestions-conf
-bin/solr create -c votes -d ~/hack/wordnet/ownpt2/solr/votes-conf
-bin/solr create -c audit -d ~/hack/wordnet/ownpt2/solr/audit-conf
+#bin/solr delete -c pointers
+#bin/solr create -c wn -d ~/hack/wordnet/ownpt2/solr/wn-conf
+#bin/solr create -c st -d ~/hack/wordnet/ownpt2/solr/st-conf
+#bin/solr create -c suggestions -d ~/hack/wordnet/ownpt2/solr/suggestions-conf
+#bin/solr create -c votes -d ~/hack/wordnet/ownpt2/solr/votes-conf
+#bin/solr create -c audit -d ~/hack/wordnet/ownpt2/solr/audit-conf
+#bin/solr create -c pointers -d ~/hack/wordnet/ownpt2/solr/pointers-conf
+
+
