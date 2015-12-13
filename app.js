257d9ff5a0bc2ed37295a14537facb5dccb5abd0
Fabricio Chalub <fcbrbr@gmail.com>
2015-12-13 08:31:08 -0200

    Bumping version number.

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ app.get('/predicates',
 app.get('/',
         function(req,res)
         {
-          res.json({version: '43-solr',
+          res.json({version: '44-solr',
                     has_key: api_key != null,
                     date: new Date() });
         });
